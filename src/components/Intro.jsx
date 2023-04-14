ebff9c152e78131b4fbeaef5e0a5606bb80e499c
Luiz <luizcvbsj@gmail.com>
2023-04-14 21:10:02 +0200

    Testing base

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,8 +4,22 @@ function Intro() {
     return (
         <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
             <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Luiz</h1>
-            <p>Softare Engineer & Back-End Developer</p>
-            <p>This is my entire bio</p>
+            <p className="text-base md:text-xl mb-3 font-medium">Software Engineer & Back-End Developer</p>
+            <p className="text-sm max-w-xl mb-6 font-bold">Currently in the 7º semester of Computer Science with the
+                university of Brazil FMU - Faculdades Metropolitanas Unidas (expected to finish at the end of 2023).
+                <br />
+                <br />
+                Representing technical assistance for Beneteau, a French sail and motorboat manufacturer, in southeast
+                Brazil(Currently working with remote assistance).
+                <br />
+                <br />
+                Studying technologies with a focus on front-end development and migrating to the IT market.
+                <br />
+                <br />
+                I have focus on direct interaction with the client and figuring out personalized solutions for daily problems.
+                <br />
+                <br />
+                My hobby is to learn new languages.</p>
         </div>
     )
 }
---
+++ src/data/portfolio.js
@@ -0,0 +1,23 @@
+export default [
+    {
+        title: 'Terminal Website',  
+        imgUrl: '',
+        stack: ['HTML', 'CSS', 'JavaScript'],
+        link: 'https:fkcodes.com',
+
+    },
+    {
+        title: 'Developer Porfolio',  
+        imgUrl: '',
+        stack: ['React', 'Tailwind', 'Vite'],
+        link: 'https:thehackershideout.com',
+
+    },
+    {
+        title: 'AI Nerf Gun',  
+        imgUrl: '',
+        stack: ['Python', 'C++', 'Robotics'],
+        link: 'https:youtu.be/1ZQYQZQZQZQ',
+
+    },
+]
